31e85c296c08f9d93b7e9acd48318fa5976fced3
MD MASUM <masum@channelassist.com>
2025-08-08 14:30:19 +0600

    Update error handler

--- Backend/CleanArchitecture.Api/Middlewares/ErrorHandlerMiddleware.cs
+++ Backend/CleanArchitecture.Api/Middlewares/ErrorHandlerMiddleware.cs
@@ -17,37 +17,41 @@ namespace CleanArchitecture.Api.Middlewares
             {
                 logger.LogError(error, "An unhandled exception occurred: {ErrorMessage}", error.Message);
                 var response = context.Response;
-                response.ContentType = "application/json";
-                var responseModel = new Response<string>() { Succeeded = false, Message = error.Message };
 
-                switch (error)
+                if (!context.Response.HasStarted)
                 {
-                    case ApiException:
-                        // custom application error
-                        response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        break;
-                    case ValidationException e:
-                        // custom application error
-                        response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        responseModel.Errors = e.Errors;
-                        break;
-                    case NotFoundException:
-                        // not found error
-                        response.StatusCode = (int)HttpStatusCode.NotFound;
-                        break;
-                    default:
-                        // unhandled error
-                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                        break;
+                    response.ContentType = "application/json";
+                    var responseModel = new Response<string>() { Succeeded = false, Message = error.Message };
+
+                    switch (error)
+                    {
+                        case ApiException:
+                            // custom application error
+                            response.StatusCode = (int)HttpStatusCode.BadRequest;
+                            break;
+                        case ValidationException e:
+                            // custom application error
+                            response.StatusCode = (int)HttpStatusCode.BadRequest;
+                            responseModel.Errors = e.Errors;
+                            break;
+                        case NotFoundException:
+                            // not found error
+                            response.StatusCode = (int)HttpStatusCode.NotFound;
+                            break;
+                        default:
+                            // unhandled error
+                            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            break;
+                    }
+                    var serializeOptions = new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                        WriteIndented = true
+                    };
+                    var result = JsonSerializer.Serialize(responseModel, serializeOptions);
+
+                    await response.WriteAsync(result);
                 }
-                var serializeOptions = new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                    WriteIndented = true
-                };
-                var result = JsonSerializer.Serialize(responseModel, serializeOptions);
-
-                await response.WriteAsync(result);
             }
         }
     }
